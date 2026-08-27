805662e494a240a74a1f6ee05b5715fd79e68e2b
Nicolas Freitas Souza <darktails076@gmail.com>
2026-08-27 11:59:02 -0300

    numsei

--- waluigi/script.js
+++ waluigi/script.js
@@ -1,3 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+        const swiper = new Swiper('.meu-carrossel', {
+            // Configurações opcionais
+            loop: true, // Faz o carrossel voltar ao início infinitamente
+
+            slidesPerView: 3,
+
+            spaceBetween: 50,
+
+            centeredSlides: true,
+
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            },
+
+            // Ativa a paginação por pontinhos
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+
+            // Ativa as setas de navegação
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            // effect: fade,
+        });
 })
